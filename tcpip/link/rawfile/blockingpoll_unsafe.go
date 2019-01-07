d849d9f9a74d6b3600add5f2423b3b4234ed807e
FlowerWrong <sysuyangkang@gmail.com>
2019-01-07 17:11:30 +0800

    fix build for darwin

--- tcpip/link/rawfile/blockingpoll_unsafe.go
+++ tcpip/link/rawfile/blockingpoll_unsafe.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build linux,!amd64,!arm64
+// +build linux,!amd64,!arm64 darwin
 
 package rawfile
 
